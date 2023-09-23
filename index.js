b1a29b971ae43dd0065ec5248fc88e1dd2ba8d1e
Rohit Khadatkar <khadatkarrohit@gmail.com>
2023-09-23 13:54:01 +0530

    update files

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var fs = require('fs');
 
 function readWriteSync() {
     console.log("reading and writing from the file synchronously");
-    const data = fs.readFileSync('./AFM-source/input.txt', { encoding: 'utf8', flag: 'r'});
+    const data = fs.readFileSync('./source/input.txt', { encoding: 'utf8', flag: 'r'});
     console.log('data has been read');
-    fs.writeFileSync('./AFM-destination/output.txt', data, {encoding: 'utf8', flag: 'w'});
+    fs.writeFileSync('./destination/output.txt', data, {encoding: 'utf8', flag: 'w'});
     console.log('Writing to the file has finished');
 }
 
 function readSyncWriteAsync() {
     console.log("reading from file sync and writing to the file async");
-    const data = fs.readFileSync('./AFM-source/input.txt', { encoding: 'utf8', flag: 'r'});
+    const data = fs.readFileSync('./source/input.txt', { encoding: 'utf8', flag: 'r'});
     console.log('data has been read sync');
-    fs.writeFile('./AFM-destination/output.txt', data, {encoding: 'utf8', flag: 'w'}, function (err, data) {
+    fs.writeFile('./destination/output.txt', data, {encoding: 'utf8', flag: 'w'}, function (err, data) {
         if(err) {
             console.log('some error has happened while writing the data');
         } else {
@@ -25,11 +25,11 @@ function readSyncWriteAsync() {
 
 function readAsyncWriteSync() {
     console.log("reading from file async and writing to the file sync");
-    fs.readFile('./AFM-source/input.txt', { encoding: 'utf8', flag: 'r'}, function(err, data) {
+    fs.readFile('./source/input.txt', { encoding: 'utf8', flag: 'r'}, function(err, data) {
         if(err) {
             console.log('Something went wrong while reading async');
         } else {
-            fs.writeFileSync('./AFM-destination/output.txt', data, {encoding: 'utf8', flag: 'w'});
+            fs.writeFileSync('./destination/output.txt', data, {encoding: 'utf8', flag: 'w'});
             console.log('Writing to the file sync has finished');
         }
     });
@@ -41,7 +41,7 @@ function readAsyncWriteSync() {
 
 function readAsyncWriteAsync() {
     console.log("reading from file async and writing to the file async");
-    fs.readFile('./AFM-source/input.txt', { encoding: 'utf8', flag: 'r'}, function(err, data) {
+    fs.readFile('./source/input.txt', { encoding: 'utf8', flag: 'r'}, function(err, data) {
         for(let i=0;i<100000000000000;i++) {
     
         }
@@ -50,7 +50,7 @@ function readAsyncWriteAsync() {
             console.log('Something went wrong while reading async');
         } else {
             console.log('reading async has finished');
-            fs.writeFile('./AFM-destination/output.txt', data, {encoding: 'utf8', flag: 'w'}, function (errWrite, dataWrite) {
+            fs.writeFile('./destination/output.txt', data, {encoding: 'utf8', flag: 'w'}, function (errWrite, dataWrite) {
                 if(errWrite) {
                     console.log('Something went wrong while writing async');
                 } else {
